b274ae97cb56bee8c04e7800c9882a7374d0ec26
BheemaModam <modambheema@gmail.com>
2025-11-07 19:23:01 +0530

    slidingWindowMaximum

--- Arrays/slidingWindow/slidingWindowMaximum.js
+++ Arrays/slidingWindow/slidingWindowMaximum.js
@@ -40,4 +40,31 @@ Constraints:
  * @param {number} k
  * @return {number[]}
  */
-var maxSlidingWindow = function (nums, k) {};
+function maxSlidingWindow(nums, k) {
+  const deque = []; // store indices
+  const result = [];
+
+  for (let i = 0; i < nums.length; i++) {
+    // remove elements out of window
+    while (deque.length && deque[0] <= i - k) {
+      deque.shift();
+    }
+
+    // remove smaller elements
+    while (deque.length && nums[deque[deque.length - 1]] < nums[i]) {
+      deque.pop();
+    }
+
+    // add current element index
+    deque.push(i);
+
+    // record max (front of deque) after first k elements
+    if (i >= k - 1) {
+      result.push(nums[deque[0]]);
+    }
+  }
+
+  return result;
+}
+const res1 = maxSlidingWindow([1, 3, 2, -1, -3, 6, 5, 3, 6, 7, 8], 3);
+console.log("Result->", res1);
