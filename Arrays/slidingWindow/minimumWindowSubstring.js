82db0ef7d088ac2b03dec57a535e177faabd6370
BheemaModam <modambheema@gmail.com>
2025-11-13 15:15:42 +0530

    minimumWindowSubsting

--- Arrays/slidingWindow/minimumWindowSubstring.js
+++ Arrays/slidingWindow/minimumWindowSubstring.js
@@ -44,7 +44,56 @@ Follow up: Could you find an algorithm that runs in O(m + n) time?
  * @param {string} t
  * @return {string}
  */
-var minWindow = function (s, t) {};
+var minWindow = function (s, t) {
+  if (s.length < t.length) return "";
+  let left = 0;
+  let right = 0;
+
+  let created = 0;
+  let ans = [-1, 0, 0];
+  let hashUniqueChars = new Map();
+  // 1.create hashmap for target and uniquechars
+  for (let char of s) {
+    // if(!hashUniqueChars.get(char)){
+    //     hashUniqueChars.set(char,(hashUniqueChars.get(char))+1);
+    // }
+    hashUniqueChars.set(char, (hashUniqueChars.get(char) ?? 0) + 1);
+  }
+  let uniqueChars = hashUniqueChars.size;
+  let hashString = new Map();
+  while (right < s.length) {
+    let current = s.charAt(right);
+
+    hashString.set(current, (hashString.get(current) ?? 0) + 1);
+    if (
+      hashUniqueChars.has(current) &&
+      hashUniqueChars.get(current) === hashString.get(current)
+    ) {
+      created++;
+    }
+    while (left <= right && created === uniqueChars) {
+      const current = s.charAt(left);
+      if (ans[0] == -1 || ans[0] >= right - left + 1) {
+        ans[0] = right - left + 1;
+        ans[1] = left;
+        ans[2] = right;
+      }
+      hashString.set(current, hashString.get(current) - 1);
+      if (
+        hashUniqueChars.has(current) &&
+        hashUniqueChars.get(current) === hashString.get(current)
+      ) {
+        created--;
+      }
+      left++;
+    }
+    right++;
+  }
+  if (ans[0] === -1) {
+    return "";
+  }
+  return s.substring(ans[1], ans[right] + 1);
+};
 
 const res1 = minWindow("ADOBECODEBANC", "ABC");
 console.log(`Result->${res1}`);
