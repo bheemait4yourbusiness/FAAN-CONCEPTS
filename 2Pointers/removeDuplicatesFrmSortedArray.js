df3cc4833269f546f3a5fe0ead410458db88dc8e
BheemaModam <modambheema@gmail.com>
2025-11-15 23:28:53 +0530

    nextPermutations-start

--- 2Pointers/removeDuplicatesFrmSortedArray.js
+++ 2Pointers/removeDuplicatesFrmSortedArray.js
@@ -56,7 +56,7 @@ nums is sorted in non-decreasing order.
  */
 var removeDuplicates = function (nums) {
   let insertIndex = 1;
-  for (let i = 0; i < nums.length - 1; i++) {
+  for (let i = 1; i < nums.length; i++) {
     if (nums[i - 1] !== nums[i]) {
       nums[insertIndex] = nums[i];
       insertIndex++;
