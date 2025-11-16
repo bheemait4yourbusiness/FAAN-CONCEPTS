381c521cc5a9614f1c1358410b450d94b69b7520
BheemaModam <modambheema@gmail.com>
2025-11-16 21:51:08 +0530

    contest-476-all-starters

---
+++ Contest/476/countDistinctIntegersAfterRemovingZeros.js
@@ -0,0 +1,36 @@
+/*
+todo: 3747. Count Distinct Integers After Removing Zeros
+Medium
+premium lock iconCompanies
+Hint
+You are given a positive integer n.
+For every integer x from 1 to n, we write down the integer obtained by removing 
+all zeros from the decimal representation of x.
+Return an integer denoting the number of distinct integers written down.
+
+ 
+
+Example 1:
+Input: n = 10
+Output: 9
+Explanation:
+The integers we wrote down are 1, 2, 3, 4, 5, 6, 7, 8, 9, 1. 
+There are 9 distinct integers (1, 2, 3, 4, 5, 6, 7, 8, 9).
+
+Example 2:
+Input: n = 3
+Output: 3
+Explanation:
+The integers we wrote down are 1, 2, 3. There are 3 distinct integers (1, 2, 3).
+
+Constraints:
+
+    1 <= n <= 1015
+
+
+*/
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var countDistinct = function (n) {};
