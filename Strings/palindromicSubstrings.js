b8d7b01f0f1009fd8c0502b050b995bc5fd7f49a
BheemaModam <modambheema@gmail.com>
2025-11-30 10:01:49 +0530

    contest-478-starter-files

--- Strings/palindromicSubstrings.js
+++ Strings/palindromicSubstrings.js
@@ -29,4 +29,21 @@ Constraints:
 1 <= s.length <= 1000
 s consists of lowercase English letters.
 
-*/ 
+*/
+var countSubstrings = function (s) {
+  let result = 0;
+  function countPalindrome(s, left, right) {
+    let count = 0;
+    while (left >= 0 && right < s.length && s[left] === s[right]) {
+      count++;
+      left--;
+      right++;
+    }
+    return count;
+  }
+  for (let i = 0; i < s.length; i++) {
+    result += countPalindrome(s, i, i);
+    result += countPalindrome(s, i, i + 1);
+  }
+  return result;
+};
